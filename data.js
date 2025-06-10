af3f43e602a537b1df8a0ca031c7f53dddbcf168
BMNbbpkk <bmnbbpkkbandungbarat@gmail.com>
2025-06-10 20:21:05 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,15 @@
-const data = [
-  {
-    "Nama Barang": "Laci Box",
-    "Kode Barang": "3050104024",
-    "Ruangan": "Umum",
-    "Koordinat": "-6.813333,107.615",
-    "Kondisi": "Rusak Berat",
-    "Terakhir Dicek": "10-6-2024"
-  },
-  {
-    "Nama Barang": "Meja",
-    "Kode Barang": "2050104024",
-    "Ruangan": "Umum",
-    "Koordinat": "-6.813333,107.615",
-    "Kondisi": "Rusak Ringan",
-    "Terakhir Dicek": "10-6-2025"
+// File: /js/data.js
+export async function getBarangData() {
+  // Ganti URL dengan link opensheet.elk.sh Anda
+  const sheetURL = 'https://opensheet.elk.sh/2PACX-1vRSym1w-sN1OxdNX-o3vpOY-x35fsrjy7zzuqaF5P83n_D9OW2YSZPTeLFubY_3Ky73MeCKUamdAQCC/DataBarang';
+  
+  try {
+    const response = await fetch(sheetURL);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Gagal memuat data:", error);
+    return []; // Return array kosong jika error
   }
-];
+}
 
