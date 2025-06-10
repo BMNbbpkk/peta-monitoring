27dffa00cae2225865613e6b15a68bcacdcb2deb
BMNbbpkk <bmnbbpkkbandungbarat@gmail.com>
2025-06-10 20:31:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 // File: /js/data.js
 export async function getBarangData() {
   // Ganti URL dengan link opensheet.elk.sh Anda
-  const sheetURL = 'https://opensheet.elk.sh/2PACX-1vRSym1w-sN1OxdNX-o3vpOY-x35fsrjy7zzuqaF5P83n_D9OW2YSZPTeLFubY_3Ky73MeCKUamdAQCC/DataBarang';
+  const sheetURL = 'https://opensheet.elk.sh/1MaHYUGQOCpFQi_xzPRttI6yJWrsXMVFhcACpfPTOg3A/DataBarang';
   
   try {
     const response = await fetch(sheetURL);
