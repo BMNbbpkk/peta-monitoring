db1d349ed40d032b15ff3a62fc6ecf0ae20e0e26
BMNbbpkk <bmnbbpkkbandungbarat@gmail.com>
2025-06-10 13:58:01 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+var lokasi = [
+  {
+    "nama": "Gudang A",
+    "lat": -6.816955,
+    "lng": 107.617119
+  },
+  {
+    "nama": "Gudang B",
+    "lat": -6.818340,
+    "lng": 107.615300
+  }
+];
